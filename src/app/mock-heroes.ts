e69f5bffacc3ea23d447c4642e6e6176631393a7
Sachin Pandey <lucky2sachin@gmail.com>
2021-03-28 15:07:57 +0545

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero} from '../app/heroes/hero'
+
+export const HEROES: Hero[] = [
+    {id:11, name: 'mizu'},
+    {id:12, name: 'flame'},
+    {id:13, name: 'magma'},
+    {id:14, name: 'bazuka'},
+    {id:15, name: 'million'},
+    {id:16, name: 'one for all'},
+    {id:17, name: 'sting'},
+    {id:18, name: 'hulk'},
+    {id:19, name: 'iron man'},
+    {id:20, name: 'falcon'}
+];
