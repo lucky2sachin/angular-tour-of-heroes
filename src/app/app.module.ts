fdbdfb16b0fcd0a3211040a25e709bdb48a0be0a
Sachin Pandey <lucky2sachin@gmail.com>
2021-04-04 00:44:35 +0545

    added dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from '../app/messages/messages.component' ;
+import { MessagesComponent } from '../app/messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component' ;
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,10 +8,11 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(private messageService: MessageService) { }
+
 getHeroes(): Observable<Hero[]>{
   const heroes = of(HEROES);
   this.messageService.add('Herostring: sachin');
   return heroes;
 }
-  constructor(private messageService: MessageService) { }
 }
